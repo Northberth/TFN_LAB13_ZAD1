bee6fd1e9932624376bc6b2ce33c5137e0d3b859
Northberth <norbertm398@gmail.com>
2023-01-29 19:08:04 +0100

    LAB13_ZAD1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,66 @@
-import React from "react";
-import RegistrationForm from "./components/RegistrationForm";
+import React, { useState, useEffect } from "react";
 
-const App = () => {
-  return (
-    <div className="App">
-      <h1>Aplikacja Rejestracja</h1>
+function GenerateData() {
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(false);
 
-      <RegistrationForm />
-    </div>
-  );
-};
-export default App;
+    useEffect(() => {
+        setLoading(true);
+        fetch("https://randomuser.me/api/?results=10")
+        .then((res) => res.json())
+        .then((data) => {
+            setUsers(data.results);
+            setLoading(false);
+        });
+    }, []);
+
+    return (
+        <div>
+        <h1>Random User Generator</h1>
+        <button onClick={() => window.location.reload()}>Generate New Users</button>
+        {loading ? (
+            <p>Loading...</p>
+        ) : (
+            <table>
+            <thead>
+                <tr>
+                <th>Picture</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Location</th>
+                </tr>
+            </thead>
+            <tbody>
+                {users.map((user) => (
+                <tr key={user.login.uuid}>
+                    <td>
+                    <img src={user.picture.thumbnail} alt="" />
+                    </td>
+                    <td>
+                    {user.name.title} {user.name.first} {user.name.last}
+                    </td>
+                    <td>{user.email}</td>
+                    <td>{user.phone}</td>
+                    <td>
+                    {user.location.city}, {user.location.country}
+                    </td>
+                </tr>
+                ))}
+            </tbody>
+            </table>
+        )}
+        </div>
+    );
+}
+
+export default function App() {
+    return (
+        <div>
+        <GenerateData />
+        </div>
+    );
+}
+
+
+
